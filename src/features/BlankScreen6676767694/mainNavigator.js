d5a13a4fbefa6c5cdc0f2a2602e1a7a5c37117ce
Crowdbotics <team@crowdbotics.com>
2020-10-10 17:11:18 +0000

    add component Blank Screen 6676767

---
+++ src/features/BlankScreen6676767694/mainNavigator.js
@@ -0,0 +1,36 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+
+import SplashScreen from "../features/SplashScreen";
+import SideMenu from './sideMenu';
+//@BlueprintImportInsertion
+import BlankScreen011236Navigator from '../features/BlankScreen011236/navigator';
+
+/**
+ * new navigators can be imported here
+ */
+
+const AppNavigator = {
+
+    //@BlueprintNavigationInsertion
+BlankScreen011236: { screen: BlankScreen011236Navigator },
+
+    /** new navigators can be added here */
+    SplashScreen: {
+      screen: SplashScreen
+    }
+};
+
+const DrawerAppNavigator = createDrawerNavigator(
+  {
+    ...AppNavigator,
+  },
+  {
+    contentComponent: SideMenu
+  },
+);
+
+const AppContainer = createAppContainer(DrawerAppNavigator);
+
+export default AppContainer;
