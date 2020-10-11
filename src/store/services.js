31a89eceb45f3dd4dcfb8e5350a48c97389d2a07
Crowdbotics <team@crowdbotics.com>
2020-10-11 09:47:45 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const chetnamobAPI = axios.create({
-  baseURL: "https://chetna-mob-dev-13034-prod.herokuapp.com/",
+  baseURL: "https://chetna-mob-dev-13034.botics.co/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function api_v1_customtext_list(action) {
